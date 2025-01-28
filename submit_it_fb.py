8e25e699bfd127356ed9fe28997011c2b768b117
LordAndrew312 <yifansu@andrew.cmu.edu>
2025-01-28 16:10:27 +0800

    run

--- submit_it_fb.py
+++ submit_it_fb.py
@@ -34,8 +34,8 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("~/pql_exp/").is_dir():
-        p = Path(f"~/pql_exp/submitit")
+    if Path("/checkpoint/").is_dir():
+        p = Path(f"/checkpoint/submitit")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
