7a56e218b69116b5d60fe60ff7dce31ca02b81b2
LordAndrew312 <yifansu@andrew.cmu.edu>
2025-01-28 15:56:19 +0800

    run

--- fb_cmd_final.sh
+++ fb_cmd_final.sh
@@ -1,10 +1,10 @@
 # Syncing commands
 
-rsync -ravzP --exclude outputs/ --exclude .env/ unnatj@grogu.ri.cmu.edu:/home/jsingla/pql /private/home/unnatjain/js/
+# rsync -ravzP --exclude outputs/ --exclude .env/ unnatj@grogu.ri.cmu.edu:/home/jsingla/pql /private/home/unnatjain/js/
 
 # Running our method
 
-cd /private/home/unnatjain/js/pql
+cd ~/pql
 conda activate pql
 export LD_LIBRARY_PATH=$(conda info --base)/envs/pql/lib:$LD_LIBRARY_PATH
 
@@ -25,16 +25,16 @@ done
 
 # Allegro and Shadow Hand
 
-for i in 16 32 48
-do
-    for env in AllegroHand ShadowHand
-    do
-        python submit_it_fb.py --num_envs=24576 \
-            --env=$env \
-            --seed $i  \
-            --batch_size=49152 \
-            --ngpus=2 \
-            --wandb-entity=jsingla \
-            --use_volta32
-    done
-done
+# for i in 16 32 48
+# do
+#     for env in AllegroHand ShadowHand
+#     do
+#         python submit_it_fb.py --num_envs=24576 \
+#             --env=$env \
+#             --seed $i  \
+#             --batch_size=49152 \
+#             --ngpus=2 \
+#             --wandb-entity=jsingla \
+#             --use_volta32
+#     done
+# done
