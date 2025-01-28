ec05bf86e65e2b863b9f7a5285d0e7ed4a5047f8
LordAndrew312 <yifansu@andrew.cmu.edu>
2025-01-28 16:01:10 +0800

    run

--- fb_cmd_test.sh
+++ fb_cmd_test.sh
@@ -1,10 +1,10 @@
 # Syncing command
 
-rsync -ravzP --exclude outputs/ --exclude .env/ unnatj@grogu.ri.cmu.edu:/home/jsingla/pql /private/home/unnatjain/js/
+# rsync -ravzP --exclude outputs/ --exclude .env/ unnatj@grogu.ri.cmu.edu:/home/jsingla/pql /private/home/unnatjain/js/
 
 # Running our method
 
-cd /private/home/unnatjain/js/pql
+cd ~/pql
 conda activate pql
 export LD_LIBRARY_PATH=$(conda info --base)/envs/pql/lib:$LD_LIBRARY_PATH
 python submit_it_fb.py --num_envs=24576 \
@@ -13,4 +13,4 @@ python submit_it_fb.py --num_envs=24576 \
     --batch_size=49152 \
     --ngpus=2 \
     --partition=devlab \
-    --wandb-entity=jsingla
+    --wandb-entity=yifansu
